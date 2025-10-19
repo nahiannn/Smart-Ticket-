b49eeb2d59a5eb827ede609d721ff0ed3cd3c1b7
nahiannn <mnahian201416@bscse.uiu.ac.bd>
2025-10-19 22:30:21 +0600

    Save all PHP files and images before merging to main

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Clear all session variables
-session_destroy(); // Destroy the session
-
-// Redirect to login page
-header("Location: login.php");
-exit;
-?>
